0917b4c50c85cfe6189f6800a2a8c8feb872a715
Paul Tang <paul.tang95@gmail.com>
2020-12-28 15:59:45 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,79 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Vestido Rojo Corto',
+            category: 'Dress',
+            image: '/images/p1.jpg',
+            price: 75,
+            countInStock: 10,
+            brand: 'Kriska',
+            rating:'5',
+            numReviews:15,
+            description:'Vestido para navidad de tela HighQuality'
+        },
+        {
+            _id:'2',
+            name:'Vestido Fergie',
+            category: 'Dress',
+            image: '/images/p2.jpg',
+            price: 70,
+            countInStock: 20,
+            brand: 'Kriska',
+            rating:'4.5',
+            numReviews:10,
+            description:'Vestido navideño perfecto para el veranot'
+        },
+        {
+            _id:'3',
+            name:'Top bombon',
+            category: 'Shirt',
+            image: '/images/p3.jpg',
+            price: 40,
+            countInStock: 0,
+            brand: 'Kriska',
+            rating:'5',
+            numReviews:10,
+            description:'Top veraniego perfecto para fiestas'
+        },
+        {
+            _id:'4',
+            name:'Enteriso Kriska Premium',
+            category: 'Enterizos',
+            image: '/images/p4.jpg',
+            price: 80,
+            countInStock: 15,
+            brand: 'Kriska',
+            rating:'5',
+            numReviews:20,
+            description:'El producto estrella de Kriska es su enterizo rojo'
+        },
+        {
+            _id:'5',
+            name:'Vestido Bella allday',
+            category: 'Dress',
+            image: '/images/p5.jpg',
+            price: 75,
+            countInStock: 0,
+            brand: 'Kriska',
+            rating:'4',
+            numReviews:5,
+            description:'Imponente vestido perfecto para tu outfit de fiestas'
+        },
+        {
+            _id:'6',
+            name:'Vestido Numbella',
+            category: 'Dress',
+            image: '/images/p6.jpg',
+            price: 60,
+            countInStock: 0,
+            brand: 'Kriska',
+            rating:'4.5',
+            numReviews:20,
+            description:'El vestido que buscabas al  un precio increible'
+        }
+
+    ]
+}
+
+ export default data;
